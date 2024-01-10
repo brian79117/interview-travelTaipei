dd75b9bca8ff64bc719adca6e53f8702cc6d8d59
Brian Yeh <brian.yeh@mores.com.tw>
2024-01-10 14:41:37 +0800

    feat: init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.travel_taipei"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.travel_taipei"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // lifecycle
+    val lifecycleVersion = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+
+    // navigation
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    // dimen
+    implementation ("com.intuit.sdp:sdp-android:1.0.6")
+
+    // log
+    val timberVersion = "5.0.1"
+    implementation ("com.jakewharton.timber:timber:$timberVersion")
+
+    // di
+    val hiltVersion = "2.44"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+
+    // api
+    val retrofitVersion = "2.9.0"
+    implementation ("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:adapter-rxjava3:$retrofitVersion")
+    val okhttpVersion = "4.12.0"
+    implementation ("com.squareup.okhttp3:logging-interceptor:$okhttpVersion")
+    val rxandroidVersion = "3.0.2"
+    implementation ("io.reactivex.rxjava3:rxandroid:$rxandroidVersion")
+    val rxjavaVersion = "3.1.8"
+    implementation ("io.reactivex.rxjava3:rxjava:$rxjavaVersion")
+
+    //image load
+    val glideVersion = "4.16.0"
+    implementation ("com.github.bumptech.glide:glide:$glideVersion")
+}
+
+kapt {
+    correctErrorTypes = true
+}
